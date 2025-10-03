9192b60442136c6c1d5d127564ae87c0bb0f0e4b
4AHITS Phillip Rubak <phillip.rubak@it.htl-hl.ac.at>
2025-10-03 11:32:56 +0200

    added issubject to tags

---
+++ EduShelf.Api/Migrations/20251003092903_AddIsSubjectToTag.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduShelf.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsSubjectToTag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSubject",
+                table: "Tags",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Documents",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 10, 3, 9, 29, 3, 105, DateTimeKind.Utc).AddTicks(1210));
+
+            migrationBuilder.UpdateData(
+                table: "Documents",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 10, 3, 9, 29, 3, 105, DateTimeKind.Utc).AddTicks(1212));
+
+            migrationBuilder.UpdateData(
+                table: "Flashcards",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 10, 3, 9, 29, 3, 105, DateTimeKind.Utc).AddTicks(1277));
+
+            migrationBuilder.UpdateData(
+                table: "Flashcards",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 10, 3, 9, 29, 3, 105, DateTimeKind.Utc).AddTicks(1279));
+
+            migrationBuilder.UpdateData(
+                table: "Quizzes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 10, 3, 9, 29, 3, 105, DateTimeKind.Utc).AddTicks(1305));
+
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "IsSubject",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "IsSubject",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "IsSubject",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "IsSubject",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "IsSubject",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "IsSubject",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 10, 3, 9, 29, 3, 105, DateTimeKind.Utc).AddTicks(1082));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 10, 3, 9, 29, 3, 105, DateTimeKind.Utc).AddTicks(1181));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSubject",
+                table: "Tags");
+
+            migrationBuilder.UpdateData(
+                table: "Documents",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 29, 7, 24, 48, 351, DateTimeKind.Utc).AddTicks(1750));
+
+            migrationBuilder.UpdateData(
+                table: "Documents",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 29, 7, 24, 48, 351, DateTimeKind.Utc).AddTicks(1755));
+
+            migrationBuilder.UpdateData(
+                table: "Flashcards",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 29, 7, 24, 48, 351, DateTimeKind.Utc).AddTicks(1809));
+
+            migrationBuilder.UpdateData(
+                table: "Flashcards",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 29, 7, 24, 48, 351, DateTimeKind.Utc).AddTicks(1812));
+
+            migrationBuilder.UpdateData(
+                table: "Quizzes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 29, 7, 24, 48, 351, DateTimeKind.Utc).AddTicks(1839));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 29, 7, 24, 48, 351, DateTimeKind.Utc).AddTicks(1636));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 9, 29, 7, 24, 48, 351, DateTimeKind.Utc).AddTicks(1721));
+        }
+    }
+}
